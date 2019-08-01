71ff2a6ba4fd049ed38f46d1dd49d18e7adf3741
sanhidalgoo <53051441+sanhidalgoo@users.noreply.github.com>
2019-08-01 13:57:21 -0500

    Update Taller3.java

--- talleres/taller03/Taller3.java
+++ talleres/taller03/Taller3.java
@@ -49,63 +49,43 @@ public class Taller3 {
             torresDeHannoiAux(n-1, intermedio, origen, destino);
             }
 	}
-		
-		
-	/**
-	*
-	* El método combinations es un auxiliar del proceso 
-	* @param s es la cadena a permutar
-	*/
-	public static void combinations(String s) { 
-		combinationsAux("", s); 
-		}
-
-	/**
-	*
-	* El método combinationsAux es el core del proceso, en el se busca conseguir
-	* las posibles permutaciones que puede tener la cadena s 
-	* @param s es la cadena a permutar
-	* @param prefix cadena auxiliar donde se guarda la cadena de salida
-	*
-	* Las cadenas de salida tienes una longitud desde 0 hasta el lenght de la cadena s
-	* por simplicidad del ejercicio no se repiten letras y el orden de la secuencia 
-	* no supone importancia, es decir, la cadena AB es igual a la cadena BA.
-	* Ej: para la cadena ABC se tiene la salida: { A, AB, ABC, AC, B, BC, C}
-	*
-	*/
-	private static void combinationsAux(String prefix, String s) {  
-		//...
-	}
-		
-		
-	/**
-	*
-	* El método permutation es un auxiliar del proceso 
-	* @param str es la cadena a permutar
-	*/
-	public static void permutation(String str) {
-		permutationAux("", str); 
-	}
 	
-	/**
-	*
-	* El método permutationAux es el core del proceso, en el se busca conseguir
-	* las posibles permutaciones que puede tener la cadena str 
-	* @param str es la cadena a permutar
-	* @param prefix cadena auxiliar donde se guarda la cadena de salida
-	*
-	* Las cadenas de salida tienes una longitud de lenght de la cadena s
-	* Ej: para la cadena AB se tiene la salida: { ABC,ACB,BAC,BCA,CAB,CBA }
-	* donde los caracteres no se repiten pero el orden tiene un papel importante.
-	*
-	*/
-	private static void permutationAux(String prefix, String str) {
-            
-            if (str.length()==0){
-            System.out.println(prefix);
-            }else{
-            permutationAux(prefix+str.substring(i,i+1), str.substring(0, i)+str.substring(i+1,str.length()));
-            }
-            
-	}
-}
+	//-------------------------------------------------------------------------------------------------------------
+	
+	 /**
+     *
+     * El método permutation es un auxiliar del proceso
+     *
+     * @param str es la cadena a permutar
+     */
+    public static void permutation(String str) {
+        permutationAux("", str);
+    }
+
+    /**
+     *
+     * El método permutationAux es el core del proceso, en el se busca conseguir
+     * las posibles permutaciones que puede tener la cadena str
+     *
+     * @param str es la cadena a permutar
+     * @param prefix cadena auxiliar donde se guarda la cadena de salida
+     *
+     * Las cadenas de salida tienes una longitud de lenght de la cadena s Ej:
+     * para la cadena AB se tiene la salida: { ABC,ACB,BAC,BCA,CAB,CBA } donde
+     * los caracteres no se repiten pero el orden tiene un papel importante.
+     *
+     */
+    private static void permutationAux(String prefix, String str) {
+      if(str.length()== 0){
+          System.out.print(prefix + ", ");
+          System.out.println(AdvancedEncryptionStandard.desencriptarArchivo(prefix)); //Imprime archivo encriptado si la contraseña es correcta
+      }else{
+          for (int i = 0; i < str.length(); i++) {
+              permutationAux(prefix+str.charAt(i), str.substring(0,i)+str.substring(i+1));
+          }
+      }
+    }
+}	
+		
+		
+	
