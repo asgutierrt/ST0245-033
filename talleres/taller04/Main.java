15ebe166a61f055bd04f56e59789bd7fde88d5f0
asgutierrt <53051423+asgutierrt@users.noreply.github.com>
2019-08-09 17:05:20 -0500

    Update Main.java

--- talleres/taller04/Main.java
+++ talleres/taller04/Main.java
@@ -21,7 +21,6 @@ public class Main {
 	        b = Taller4.arrayMax(new int[] {3,2,343,2,43,55,67,68,86,3,4},10);
 	        c = Taller4.arrayMax(new int[] {56,7,6,45,8,4,34,8,7,5,34,7,78,9},13);
 	        d = Taller4.arrayMax(new int[] {1,2,3,4,5,5},5);
-	        System.out.println(a);
 	        if(a!=475 || b!=676 || c!=308 || d!=20)
 	            return false;
 	        return true;
