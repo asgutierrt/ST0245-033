86662deae32925862f9bdcba2632d9eb816af876
sanhidalgoo <53051441+sanhidalgoo@users.noreply.github.com>
2019-08-15 11:35:23 -0500

    Update Taller5.java

--- talleres/taller05/Taller5.java
+++ talleres/taller05/Taller5.java
@@ -5,12 +5,6 @@ import java.util.Arrays;
 
 public class Taller5 {
     
-    public static void main(String[] args) {
-        int [] a = {12,15,1,16,2};
-        System.out.println(Arrays.toString(a));
-        insertion(a);
-        //System.out.println(Arrays.toString(a));
-    }
     public static void insertion(int [] a){
         for (int i = 1; i < a.length; i++) {
             int auxiliar = a[i];
@@ -21,7 +15,6 @@ public class Taller5 {
                 a[i] = temporal;
                 i--;
                 j--;
-                System.out.println(Arrays.toString(a));
                 if(j==-1)break;
                 if(i==-1)break;
             }
