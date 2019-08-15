1a49db6c529f5273d4ef27fc9f03cbdedd5e82ce
sanhidalgoo <53051441+sanhidalgoo@users.noreply.github.com>
2019-08-15 13:18:04 -0500

    Update Main.java

--- talleres/taller05/Main.java
+++ talleres/taller05/Main.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package taller5;
 
 import java.util.Random;
 
-/**
- *
- * @author asgutierrt
- */
 public class Main {
     public static boolean ejercicio1(){
         int a,b,c,d;
