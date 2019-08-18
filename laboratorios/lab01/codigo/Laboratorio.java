1c1a41d8a20e56194cc2fc23aa6576e6471992d9
asgutierrt <53051423+asgutierrt@users.noreply.github.com>
2019-08-18 00:08:10 -0500

    Update Laboratorio.java

--- laboratorios/lab01/codigo/Laboratorio.java
+++ laboratorios/lab01/codigo/Laboratorio.java
@@ -9,41 +9,39 @@ package laboratorio;
 public class Laboratorio {
 
     /**
-     * Calcula la longitud de la subsecuencia común más larga a dos cadenas de
-     * caracteres.
+     * 
+     * Calculates the length of the longest common subsequence to two chains of
+     * characters.
      *
      *
-     * @param x Cadena de caracteres
-     * @param y Cadena de caracteres
-     * @return Longitud de la subsecuencia común más larga a dos cadenas de
-     * caracteres.
+     * @param x chain of characters.
+     * @param y chain of characters.
+     * @return length of the longest common subsequence of two chains of characters.
      */
     public static int lcs(String x, String y) {
         if (x.length() == 0 || y.length() == 0) {   
-            return 0;                                // C_1
+            return 0;                               // C_1
         } else if (x.charAt(x.length() - 1) == y.charAt(y.length() - 1)) {
-            return lcs(x.substring(0, x.length() - 1), y.substring(0, y.length() - 1)) + 1;  
+            return lcs(x.substring(0, x.length() - 1), y.substring(0, y.length() - 1)) + 1;  // 
         } else {
-            return Math.max(lcs(x.substring(0, x.length() - 1), y), lcs(x, y.substring(0, y.length() - 1)));
+            return Math.max(lcs(x.substring(0, x.length() - 1), y), lcs(x, y.substring(0, y.length() - 1)));   //
         }
     }
 
     /**
-     * Realiza el calculo de las formas en las cuales se pueden ubicar los
-     * rectangulos 1x2 en otro 2xn ademas dibuja cada una de ellas
+     * Makes the calculation of the ways in which you can place 1x2 rectangles in 2xn ones.
      *
-     * @param n es el largo del rectangulo que se debe de utilizar para calcular
-     * el numero de formas de ubicar el rectangulo 1x2
-     * @return int retorna un entero que representa las formas en las que se
-     * puede llenar el rectangulo 2xn
+     * @param n is the length of the rectangle that should be used to calculate the number of ways to locate the 1x2 rectangles 
+     * @return  ways in which you can fill the 2xn rectangles
+     * 
      */
     public static int nRectangulos(int n) {
         if (n == 1) {
             return 1;   //C_1
         }
         if (n == 2) {
-            return 2; //C_2
+            return 2;   //C_2
         }
-        return nRectangulos(n - 1) + nRectangulos(n - 2); //T(n) = C_3 T(n-1) + T(n-2)
+        return nRectangulos(n - 1) + nRectangulos(n - 2);  //T(n) = C_3 T(n-1) + T(n-2)
     }
 }
