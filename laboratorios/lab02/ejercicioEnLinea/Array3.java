c0a82271e75cc5a8416995dac5f6b2e6d4768e34
sanhidalgoo <53051441+sanhidalgoo@users.noreply.github.com>
2019-08-25 16:59:27 -0500

    Update Array3.java

--- laboratorios/lab02/ejercicioEnLinea/Array3.java
+++ laboratorios/lab02/ejercicioEnLinea/Array3.java
@@ -18,7 +18,7 @@ public class Array3 {
      * @return  the largest span found in the given array.
      */
     public static int maxSpan(int[] nums) {
-        if (nums.length > 0 && nums[0] != nums[nums.length - 1]) {
+        if (nums.length > 0 && nums[0] != nums[nums.length - 1]) {  //T(n) = 
             return nums.length - 1;
         }
         return nums.length;
@@ -33,20 +33,19 @@ public class Array3 {
      * side.
      */
     public static boolean canBalance(int[] nums) {
-        int sumaIzquierda = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            sumaIzquierda += nums[i];
-            int sumaDerecha = 0;
-            for (int j = nums.length - 1; i < j; j--) {
-                sumaDerecha += nums[j];
+        int sumaIzquierda = 0;                      //T(n) = C1
+        for (int i = 0; i < nums.length; i++) {     //T(n) = C2*n
+            sumaIzquierda += nums[i];               //T(n) = C3*n
+            int sumaDerecha = 0;                    //T(n) = C4*n 
+            for (int j = nums.length - 1; i < j; j--) { //T(n) = C5*n*n
+                sumaDerecha += nums[j];                 //T(n) = C6*n*n
             }
-            if (sumaIzquierda == sumaDerecha) {
-                return true;
+            if (sumaIzquierda == sumaDerecha) {     //T(n) = C7*n
+                return true;                         //T(n) = C8*n
             }
         }
-        return false;
-    }
+        return false;                            //T(n) = C9
+    }                                           //T(n) = (C1+C8) + (C2+C3+C4)*n + (C5+C6)*n^2 = C*n^2 + C*n +C
 
     /**
      * 3. seriesUp
@@ -55,15 +54,15 @@ public class Array3 {
      * @return an array with the pattern {1, 1, 2, 1, 2, 3, ... 1, 2, 3 .. n}
      */
     public static int[] seriesUp(int n) {
-        int cont = 0;
-        int[] array = new int[n * (n + 1) / 2];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                array[cont++] = j;
+        int cont = 0;                           //T(n) = C1
+        int[] array = new int[n * (n + 1) / 2]; //T(n) = C2
+        for (int i = 1; i <= n; i++) {          //T(n) = C3*n
+            for (int j = 1; j <= i; j++) {      //T(n) = C4*n*n
+                array[cont++] = j;              //T(n) = C5*n*n
             }
         }
-        return array;
-    }
+        return array;                            //T(n) = C6
+    }                                           //T(n) = (C1+C2+C6) + (C3)*n + (C4+C5)*n*n = C*n^2 + C*n +C
 
     /**
      * 4. countClumps
@@ -75,17 +74,17 @@ public class Array3 {
      * @return Return the number of clumps in the given array.
      */
     public static int countClumps(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                count++;
+        int count = 0;                                   //T(n) = C1
+        for (int i = 0; i < nums.length - 1; i++) {      //T(n) = C2*n
+            if (nums[i] == nums[i + 1]) {                 //T(n) = C3*n
+                count++;                                    //T(n) = C4*n
             }
-            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-                i++;
+            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {  //T(n) = C5*n*n
+                i++;                                                 //T(n) = C6*n*n
             }
         }
-        return count;
-    }
+        return count;                                                //T(n) = C7
+    }                                                              //T(n) = (C5+C6)*n^2 +(C2+C3+C4)*n+(C1+C7) = C*n^2 + C*n +C
 
     /**
      * 5. fix34
@@ -97,16 +96,16 @@ public class Array3 {
      *
      */
     public int[] fix34(int[] nums) {
-        int aux = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == 4 && nums[j] == 3) {
-                    aux = nums[j + 1];
-                    nums[j + 1] = nums[i];
-                    nums[i] = aux;
+        int aux = 0;                                     //T(n) = C1
+        for (int i = 0; i < nums.length; i++) {            //T(n) = C2*n
+            for (int j = 0; j < nums.length; j++) {         //T(n) = C3*n*n
+                if (nums[i] == 4 && nums[j] == 3) {           //T(n) = C4*n*n
+                    aux = nums[j + 1];                        //T(n) = C5*n*n
+                    nums[j + 1] = nums[i];                      //T(n) = C6*n*n
+                    nums[i] = aux;                                   //T(n) = C7*n*n
                 }
             }
         }
-        return nums;
-    }
+        return nums;                                  //T(n) = C8
+    }                               //T(n) = (C3+C4+C5+C6)n^2 + (C2)*n +(C1+C8) = C*n^2 + C*n +C
 }
