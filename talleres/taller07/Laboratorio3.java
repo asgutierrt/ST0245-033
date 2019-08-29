082b009fca42c048849ccd2ab6a1907b15a28534
asgutierrt <53051423+asgutierrt@users.noreply.github.com>
2019-08-29 11:52:57 -0500

    Add files via upload

---
+++ talleres/taller07/Laboratorio3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller7;
+
+public class Laboratorio3 {
+public static void main(String[] args)
+{
+	
+	LinkedListMauricio list = new LinkedListMauricio();
+	list.insert(5, 0);
+	list.insert(4, 0);
+	list.insert(3, 0);
+	list.insert(2, 0);
+	list.insert(1, 0);
+	for (int i = 0; i < list.size(); i++)
+	{
+		System.out.println(list.get(i));
+	}
+	
+	System.out.println(list.contains(3));
+	list.remove(2);
+	System.out.println(list.contains(3));
+	for (int i = 0; i < list.size(); i++)
+	{
+		System.out.println(list.get(i));
+	}
+}
+	
+}
