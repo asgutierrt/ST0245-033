7a36dd9025920167fa41b777c236ac945d2400e8
sanhidalgoo <53051441+sanhidalgoo@users.noreply.github.com>
2019-10-03 10:33:32 -0500

    Add files via upload

---
+++ talleres/taller10/Main.java
@@ -0,0 +1,14 @@
+package Taller10;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinaryTree a = new BinaryTree(new Node(3));
+        int[] b = {67, 1, 4, 8, 2, 5, 9};
+        for (int i = 0; i < b.length; i++) {
+            a.insertar(b[i]);
+        }
+        System.out.println(a.buscar(3));
+        a.recursivePrint();
+    }
+}
